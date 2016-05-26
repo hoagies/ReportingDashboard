aa151cf16a4774cbc34d3d81541072475849c4ee
hoagies <sphogan85@gmail.com>
2016-05-26 17:04:46 +0000

    added user stories blocker panel

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ Ext.define('CustomApp', {
 
 		this.add({
 			xtype: 'rallycombobox',
+			id: 'comboInitiative',
 			stateful: true,
 			stateId: this.getContext().getScopedStateId('initiative'),
 			width: 600,
@@ -53,7 +54,7 @@ Ext.define('CustomApp', {
         
         var panelBlockers = new Ext.Panel({
             id: 'panelBlockers',
-            title: 'Blockers',
+            title: 'Blockers - User Stories',
             width: 1000,
             height: 300,
             overflowY: 'auto',
@@ -64,6 +65,10 @@ Ext.define('CustomApp', {
     },
     
     _onLoad: function(a) {
+    	
+    	// ***************************************
+    	// Load Initiative Description Panel
+    	// ***************************************
         var combo = this.down('rallycombobox');
         var initiativeDesc = combo.getRecord().get("Description");
 
@@ -73,9 +78,92 @@ Ext.define('CustomApp', {
 
 		panelDesc.add({
 		  xtype: 'box',
-		  autoEl: {id: 'myelement', cn: initiativeDesc}
+		  autoEl: {cn: initiativeDesc}
 		});
         
-    }
+        // ***************************************
+    	// Load Blockers Panel
+    	// ***************************************
+  //      if (this.down('#panelBlockers')) {
+		// 	this.down('#panelBlockers').destroy();
+		// }
+		var panelBlockers = that.down('#panelBlockers');
+		panelBlockers.removeAll();
+
+		panelBlockers.add({
+		// this.add({
+			xtype: 'rallygrid',
+			header: false,
+			width: '95%',
+			columnCfgs: [
+				{
+					text: 'ID', dataIndex: 'FormattedID', flex: 10
+				},
+				{
+					text: 'Name', dataIndex: 'Name', flex: 30
+				},
+				{
+					text: 'Blocked', dataIndex: 'Blocked', flex: 30
+				},
+				{
+					text: 'Blocked Reason', dataIndex: 'BlockedReason', flex: 30
+				}
+			],
+			enableEditing: false,
+			showRowActionsColumn: false,
+			showPagingToolbar: false,
+			storeConfig: {
+				model: 'userstory',
+				pageSize: 200,
+				filters: [that._getFilterGrid()]
+			}
+			// ******************************
+			// For Tooltip
+			// ******************************
+			// listeners: {
+			// 	itemclick: function(g, record, item) {
+			// 		tooltipvalue = record.get('FormattedID') + ' - ' + record.get('Name');
+			// 		Ext.create('Rally.ui.tooltip.ToolTip', {
+			// 			target : item,
+			// 			destroyAfterHide: true,
+			// 			// hideDelay: 0,
+			// 			html: '<p><strong>' + tooltipvalue + '</strong></p>'
+			// 		}).showNow();
+			// 	}
+			// 	// select: this._getRecordOnSelectedRow,
+			// 	// load : function(g, record, index, options){
+			// 		// this._getRecordOnSelectedRow(g, record, 0, options);
+			// 	// },
+			// 	// scope: this
+			// }
+			
+		});
+        
+    },
+    
+    // _getFilterGrid: function(record,id) {
+    _getFilterGrid: function() {
+		var filters = Ext.create('Rally.data.QueryFilter', {
+			property: 'Blocked',
+			operator: '=',
+			value: true
+		});
+		
+		var comboInitiative = this.down('#comboInitiative');
+		var initiativeID = comboInitiative.getRecord().get('FormattedID');
+		filters = filters.and(Ext.create('Rally.data.QueryFilter', {
+			property: 'Feature.Parent.Parent.FormattedID',
+			operator: '=',
+			value: initiativeID
+		}));
+		
+		// filters = filters.and(Ext.create('Rally.data.QueryFilter', {
+		// 	property: 'Project.Name',
+		// 	operator: '=',
+		// 	value: record
+		// }));
+
+		return filters;
+	},
     
 });
